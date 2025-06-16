1b5304793e9e27704df645c7e3613db1cc1002fd
ZunoBotics <jjingofarouq@gmail.com>
2025-06-16 06:17:06 +0300

    Create Footer.js

---
+++ ob/components/Footer.js
@@ -0,0 +1,10 @@
+project/components/Footer.js
+export default function Footer() {
+  return (
+    <footer className="bg-surface shadow-md py-4 mt-auto">
+      <div className="container mx-auto px-4 text-center text-text-secondary">
+        <p>&copy; {new Date().getFullYear()} Grant Website. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
