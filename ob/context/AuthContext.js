a4ee71a28014ce39c5bb4bfc839c9bab8323fd62
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-17 02:17:49 +0300

    Update AuthContext.js

--- ob/context/AuthContext.js
+++ ob/context/AuthContext.js
@@ -1,19 +1,25 @@
+// context/AuthContext.js
+'use client';
 import { createContext, useContext, useState, useEffect } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../lib/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, setUser);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
     return () => unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, loading }}>
       {children}
     </AuthContext.Provider>
   );
