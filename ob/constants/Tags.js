697d9f2feccfe7baa3e3308c8542bd5ca7f47191
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-17 08:12:59 +0300

    Update Tags.js

--- ob/constants/Tags.js
+++ ob/constants/Tags.js
@@ -1,4 +1,4 @@
-export const tagsOptions = [
+export const manytags = [
   { value: 'sustainability', label: 'Sustainability' },
   { value: 'innovation', label: 'Innovation' },
   { value: 'education', label: 'Education' },
