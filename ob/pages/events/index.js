f215316ba68fa3b98733f2bbe426ecffcbfd1cb5
ZunoBotics <jjingofarouq@gmail.com>
2025-06-17 19:01:43 +0300

    Update index.js

--- ob/pages/events/index.js
+++ ob/pages/events/index.js
@@ -1,6 +1,7 @@
+// EventsPage.jsx
 import { useState, useEffect } from 'react';
 import GrantCard from '../../components/GrantCard';
-
+import styles from './EventsPage.module.css';
 
 export default function EventsPage() {
   const [grants, setGrants] = useState([]);
